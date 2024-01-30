8f105127a2c0c6863b49aa816b11dfad7ae0c071
Deimer Roncancio <deimerroncancio@gmail.com>
2024-01-30 10:44:58 -0500

    refactor: archivo Provider.jsx documentado.

--- src/app/Provider.jsx
+++ src/app/Provider.jsx
@@ -1,7 +1,8 @@
-"use client"
-
+// Importar el proveedor de sesión de NextAuth
 import { SessionProvider } from "next-auth/react"
 
+// Componente de proveedores para envolver la aplicación con el proveedor de sesión
 export default function Providers({ children }) {
+    // Devolver el componente SessionProvider que envuelve a los hijos
     return <SessionProvider>{children}</SessionProvider>
 }
